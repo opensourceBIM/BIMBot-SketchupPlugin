847611f8076e8f63accf6f2dbc0bcebf4519a658
Jan Brouwer <jan@brewsky.nl>
2015-01-30 23:04:02 +0100

    Improved some formatting

--- brewsky/BIMserver/core.rb
+++ brewsky/BIMserver/core.rb
@@ -1,6 +1,6 @@
 #  core.rb
 #  
-#  Copyright 2015 Jan <Jan@ASUS>
+#  Copyright 2015 Jan Brouwer <jan@brewsky.nl>
 #  
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
